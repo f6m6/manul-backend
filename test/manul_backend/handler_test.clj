8a1b98bd40cb9c92d75611b0699fc5bdb4541351
Farhan Mannan <farhanmannan@mac.com>
2017-06-13 19:19:01 +0100

    Create default lein compojure app

---
+++ test/manul_backend/handler_test.clj
@@ -0,0 +1,14 @@
+(ns manul-backend.handler-test
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [manul-backend.handler :refer :all]))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (mock/request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "not-found route"
+    (let [response (app (mock/request :get "/invalid"))]
+      (is (= (:status response) 404)))))
