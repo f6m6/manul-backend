b6e0857bc3389696b7a07d220feae56df69d260d
Farhan Mannan <farhanmannan@mac.com>
2017-07-17 21:32:27 +0100

    Display next-active-songs-html on GET /

--- src/manul_backend/handler.clj
+++ src/manul_backend/handler.clj
@@ -18,6 +18,7 @@
                :password (System/getenv "GIGS_PASSWORD")})
 
 (defentity view_songs_per_date)
+(defentity next_song)
 
 (defn format-day
   "Takes 0 to -, takes 10 to a"
@@ -36,7 +37,20 @@
         (map format-day)
         ((fn [x] (concat (vec x) [" " (bar n) " " (if (pos? n) n)])))
         (s/join))))
-  
+
+(defn next-active-songs-html
+  "Dump it out"
+  [& args]
+  (let [rows (select next_song)]
+   (->> rows
+        (map
+         (fn
+           [row]
+           (let [{:keys [song_id count last_played ]} row]
+            (str (format "%-20s" song_id) (format "%-9s" count) last_played " ago\n"))))
+        (into ["<pre>A c t i v e    S o n g s\n\nNAME                PLAYS    LAST PLAYED\n"])
+        (apply str))))
+
 (defn songs
   "Dump it out"
   [& args]
@@ -48,7 +62,9 @@
         (map vec)
         (map format-row)
         (into ["<pre>MTWTFSS"])
-        (s/join "<br />"))))
+        (s/join "<br />")
+        (str (doall (next-active-songs-html))))))
+
 
 (defroutes app-routes
   (GET "/" [] songs)
