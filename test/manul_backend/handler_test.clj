c2ec7fa81aef79645283300b554b17c52f5504a9
Farhan Mannan <farhanmannan@mac.com>
2026-02-01 23:18:43 +0000

    Test update song length normalization

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -344,6 +344,22 @@
         (is (= 200 (:status response)))
         (is (= "00:02:05" (nth (:params @inserted) 4)))))))
 
+(deftest update-song-normalizes-short-length
+  (let [calls (atom [])]
+    (with-redefs [with-transaction (fn [f] (f))
+                  korma/exec-raw (fn [& args]
+                                   (let [[sql params] (if (vector? (first args))
+                                                       (first args)
+                                                       (second args))]
+                                     (swap! calls conj {:sql sql :params params})
+                                     [{:title "Song A"}]))]
+      (let [body (json/write-str {:length "2:05"})
+            response (update-song "Song A" (-> (mock/request :put "/songs/Song%20A" body)
+                                               (mock/content-type "application/json")))]
+        (is (= 200 (:status response)))
+        (let [update (first @calls)]
+          (is (= "00:02:05" (nth (:params update) 3))))))))
+
 (deftest create-song-inserts-album-association
   (let [calls (atom [])]
     (with-redefs [with-transaction (fn [f] (f))
