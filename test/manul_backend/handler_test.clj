21fe16682765da058094d9cf2c3e4537f5ba9c65
Farhan Mannan <farhanmannan@mac.com>
2026-02-01 23:36:56 +0000

    Test invalid gig type on create

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -64,6 +64,13 @@
         (is (= 0 (nth (:params @performance-insert) 4)))
         (is (= "showcase" (nth (:params @performance-insert) 5)))))))
 
+(deftest create-performance-rejects-invalid-gig-type
+  (let [body (json/write-str {:venue "The Place" :songs ["Song A"] :gig_type "invalid_type"})
+        response (create-performance (-> (mock/request :post "/create-performance" body)
+                                         (mock/content-type "application/json")))]
+    (is (= 400 (:status response)))
+    (is (re-find #"gig_type is invalid" (:body response)))))
+
 (deftest update-performance-rejects-invalid-gig-type
   (with-redefs [korma/exec-raw (fn [& _] [])]
     (let [body (json/write-str {:venue "The Place" :date "2026-02-01" :gig_type "invalid_type"})
