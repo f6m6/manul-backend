207476bcd95992acbd4f6bdef5f4dc63426723cf
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:28:21 +0000

    Test practice session requires date

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -627,6 +627,13 @@
       (is (= 400 (:status response)))
       (is (re-find #"songs are required" (:body response))))))
 
+(deftest update-practice-session-requires-date
+  (let [body (json/write-str {:date "" :songs ["Song A"]})
+        response (update-practice-session "42" (-> (mock/request :put "/practice-sessions/42" body)
+                                                   (mock/content-type "application/json")))]
+    (is (= 400 (:status response)))
+    (is (re-find #"date is required" (:body response)))))
+
 (deftest update-practice-session-returns-404-when-missing
   (with-redefs [with-transaction (fn [f] (f))
                 korma/exec-raw (fn [& args]
