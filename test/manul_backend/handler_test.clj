2d258f2e02245bfeebfcdecdd2069588d43bf1a9
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:20:14 +0000

    Test fee normalization numeric

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -674,6 +674,9 @@
 (deftest normalize-fee-micro-gbp-handles-numeric-string
   (is (= 123 (normalize-fee-micro-gbp "123"))))
 
+(deftest normalize-fee-micro-gbp-handles-number
+  (is (= 123 (normalize-fee-micro-gbp 123))))
+
 (deftest normalize-gig-type-trims-and-rejects-blank
   (is (= "open_mic" (normalize-gig-type "  open_mic  ")))
   (is (nil? (normalize-gig-type "")))
