84e72bc5ee1f5dfa34d3f993f7a3e1966b0522a7
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:58:50 +0000

    Reject non-string practice songs before db

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -730,6 +730,14 @@
     (is (= 400 (:status response)))
     (is (re-find #"songs are required" (:body response)))))
 
+(deftest update-practice-session-rejects-non-string-song-before-db
+  (with-redefs [with-transaction (fn [_] (throw (ex-info "should not hit db" {})))]
+    (let [body (json/write-str {:date "2026-02-01" :songs [123]})
+          response (update-practice-session "42" (-> (mock/request :put "/practice-sessions/42" body)
+                                                   (mock/content-type "application/json")))]
+      (is (= 400 (:status response)))
+      (is (re-find #"songs are required" (:body response))))))
+
 (deftest update-practice-session-returns-404-when-missing
   (with-redefs [with-transaction (fn [f] (f))
                 korma/exec-raw (fn [& args]
