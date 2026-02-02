7ea27c5da25efc40547411665e56fceb24dad445
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:29:14 +0000

    Test practice session blank songs

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -627,6 +627,15 @@
       (is (= 400 (:status response)))
       (is (re-find #"songs are required" (:body response))))))
 
+(deftest create-practice-session-rejects-blank-songs
+  (with-redefs [with-transaction (fn [f] (f))
+                korma/exec-raw (fn [& _] :ok)]
+    (let [body (json/write-str {:date "2026-02-01" :songs [" " "\t"]})
+          response (create-practice-session (-> (mock/request :post "/practice-sessions" body)
+                                                (mock/content-type "application/json")))]
+      (is (= 400 (:status response)))
+      (is (re-find #"songs are required" (:body response))))))
+
 (deftest update-practice-session-requires-date
   (let [body (json/write-str {:date "" :songs ["Song A"]})
         response (update-practice-session "42" (-> (mock/request :put "/practice-sessions/42" body)
