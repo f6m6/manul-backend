d966b9f882cb23ca5772887a24798c049ae12b0c
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:15:22 +0000

    Test fee normalization rejects non-numeric

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -650,6 +650,9 @@
 (deftest normalize-fee-micro-gbp-trims-input
   (is (= 1200000 (normalize-fee-micro-gbp "  1200000  "))))
 
+(deftest normalize-fee-micro-gbp-rejects-non-numeric
+  (is (thrown? NumberFormatException (normalize-fee-micro-gbp "abc"))))
+
 (deftest normalize-gig-type-trims-and-rejects-blank
   (is (= "open_mic" (normalize-gig-type "  open_mic  ")))
   (is (nil? (normalize-gig-type "")))
