caeba41ec4c6bf00b664f320e404f4e67e474532
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:02:48 +0000

    Test delete singing lesson missing

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -397,6 +397,19 @@
       (is (= 200 (:status response)))
       (is (= 9 (:id body))))))
 
+(deftest delete-singing-lesson-returns-404-when-missing
+  (with-redefs [with-transaction (fn [f] (f))
+                korma/exec-raw (fn [& args]
+                                 (let [[sql params] (if (vector? (first args))
+                                                     (first args)
+                                                     (second args))]
+                                   (if (re-find #"delete from singing_lessons" sql)
+                                     []
+                                     :ok)))]
+    (let [response (delete-singing-lesson "999")]
+      (is (= 404 (:status response)))
+      (is (re-find #"singing lesson not found" (:body response))))))
+
 (deftest create-practice-session-inserts-song-id
   (let [calls (atom [])]
     (with-redefs [with-transaction (fn [f] (f))
