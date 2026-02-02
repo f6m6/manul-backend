1e30c4703eb372fc66050cf29c0d24c9bd31815f
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:05:14 +0000

    Test delete venue missing

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -436,6 +436,18 @@
       (is (= 404 (:status response)))
       (is (re-find #"performance not found" (:body response))))))
 
+(deftest delete-venue-returns-404-when-missing
+  (with-redefs [korma/exec-raw (fn [& args]
+                                 (let [[sql params] (if (vector? (first args))
+                                                     (first args)
+                                                     (second args))]
+                                   (if (re-find #"delete from venues" sql)
+                                     []
+                                     :ok)))]
+    (let [response (delete-venue "Missing Venue")]
+      (is (= 404 (:status response)))
+      (is (re-find #"venue not found" (:body response))))))
+
 (deftest create-practice-session-inserts-song-id
   (let [calls (atom [])]
     (with-redefs [with-transaction (fn [f] (f))
