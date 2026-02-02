d83ee1bfd3d960724b0dafe176fa3d012462b992
Farhan Mannan <farhanmannan@mac.com>
2026-02-02 00:21:48 +0000

    Test length normalization 0:59

--- test/manul_backend/handler_test.clj
+++ test/manul_backend/handler_test.clj
@@ -656,6 +656,9 @@
 (deftest normalize-length-interval-keeps-non-time-string
   (is (= "unknown" (normalize-length-interval "unknown"))))
 
+(deftest normalize-length-interval-expands-mmss-with-leading-zero
+  (is (= "00:00:59" (normalize-length-interval "0:59"))))
+
 (deftest normalize-fee-micro-gbp-parses-strings
   (is (= 1200000 (normalize-fee-micro-gbp "1200000")))
   (is (= 1200000 (normalize-fee-micro-gbp 1200000)))
